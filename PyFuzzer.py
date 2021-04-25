1238cc402b778e6dec40fd5bf0f6142d01e50c2e
Jacob Collins <jacobc1926@gmail.com>
2021-04-25 10:37:25 +1000

    Added todo

---
+++ PyFuzzer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from PyNetTools.PyNetTools import *
+from PyPrintSystem.PyPrintSystem import *
+p("ToDo:", 'i')
+p("- Scan network (or add host manually)", 'i')
+p("- Port scan host", 'i')
+p("- Select ports to fuzz, or all", 'i')
+p("- Send all data to port, or autodetect service")
+p("- Print responses to STDOUT or to log file")
+p("- Jacob, you need to dump network data, like http packets and whatnot, and save them to a temporary directory, to analyze!", 'w')
