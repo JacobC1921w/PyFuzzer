7c2a6f9fe1a9be97838547d505755f29edeee0c2
Jacob Collins <jacobc1926@gmail.com>
2021-04-25 19:45:03 +1000

    Added to PyFuzzer.py

--- PyFuzzer.py
+++ PyFuzzer.py
@@ -2,9 +2,10 @@
 
 from PyNetTools.PyNetTools import *
 from PyPrintSystem.PyPrintSystem import *
-from sys import argv
-from os import linesep
+from sys import argv, maxsize
+from os import linesep, path
 from time import sleep as delay
+from random import randint
 
 def doHelp(errorMessage, exitCode=1, verbose=False):
     if(exitCode != 0):
@@ -15,12 +16,12 @@ def doHelp(errorMessage, exitCode=1, verbose=False):
     
     p("<opts>:")
     p("\t-(-h)elp\t\t\tShow this help message")
-    p("\t-(-v)erbose\t\t\tTurns on verbose messaging (like this one!)", 'v', True)
-    p("\t-(t)arget-mode <targetMode>\tTarget mode to use (see <targetMode>)")
+    p("\t-(t)arget <target>\tTarget mode to use (see <target>)")
     p("\t-(-o)utput <outputMode>\t\tOutput mode to use (see <outputMode>)")
+    p("\t-(-v)erbose\t\t\tTurns on verbose messaging (like this one!)", 'v', True)
     print()
 
-    p("<targetMode>:")
+    p("<target>:")
     p("\tscan\t\t\t\tScans the networks subnet for online hosts")
     p("\t<ip>\t\t\t\tSkips the scan and uses the specified <ip> instead (FASTER)")
     print()
@@ -35,6 +36,8 @@ def doHelp(errorMessage, exitCode=1, verbose=False):
 
 verbose = False
 skipThisIteration = False
+seed = str(randint(-int(maxsize)-1,int(maxsize)))
+onlineHosts = 0
 
 if len(argv) == 1:
     doHelp("Argumental error", 1)
@@ -53,32 +56,62 @@ else:
         elif option.lower() in ["-v", "--verbose"]:
             p("Setting verbose to true", 'v', verbose)
             verbose = True
-        elif option.lower() in ["-t", "--target-mode"]:
+        elif option.lower() in ["-t", "--target"]:
             p("Setting target mode", 'v', verbose)
             skipThisIteration = True
             try:
                 if commandLineOptions[commandLineOptions.index(option) + 1].lower() == "scan":
                     p("Target mode set to scan", 'v', verbose, "", linesep + linesep)
-                    targetHost = ""
+                    targetHost = seed + "scan" + seed
                 else:
                     p("Target host set manually to " + commandLineOptions[commandLineOptions.index(option) + 1], 'v', verbose, "", linesep + linesep)
                     targetHost = commandLineOptions[commandLineOptions.index(option) + 1]
             except IndexError:
-                doHelp("Expected <targetMode> after: " + option, 2, verbose)
+                doHelp("Expected <targetMode> after: " + option, 1, verbose)
         elif option.lower() in ["-o", "--out"]:
             p("Setting output mode", 'v', verbose)
             skipThisIteration = True
             try:
                 if commandLineOptions[commandLineOptions.index(option) + 1].lower() == "stdout":
                     p("Output mode set to STDOUT", 'v', verbose, "", linesep + linesep)
-                    outputFile = "STDOUT"
+                    outputFile = seed + "STDOUT" + seed
                 else:
                     p("Outputting to file: " + commandLineOptions[commandLineOptions.index(option) + 1], 'v', verbose, "", linesep + linesep)
                     outputFile = commandLineOptions[commandLineOptions.index(option) + 1]
             except IndexError:
-                doHelp("Expected <outputMode> after: " + option, 3, verbose)
+                doHelp("Expected <outputMode> after: " + option, 2, verbose)
+        else:
+            doHelp("Unexpected option: " + option, 3)
+
+p("Verifying CL arguments", 'v', verbose)
+if targetHost != (seed + "scan" + seed):
+    if ping(targetHost, 5):
+        p(targetHost + " is up!", 's')
+    else:
+        doHelp(targetHost + " doesn't appear to be online", 4, verbose)
+
+if outputFile != (seed + "STDOUT" + seed):
+    if path.isfile(outputFile):
+        p(outputFile + " exists, appending to file")
+    else:
+        p(outputFile + " doesn't exist, creating it")
+        with open(outputFile, "w+") as f:
+            f.write("# Start of " + outputFile)
+
+if targetHost == (seed + "scan" + seed):
+    for host in (parseScan(hostScan(getPrivateIP()), getPrivateIP())[0]):
+        onlineHosts += 1
+        p(host + " was found online", 's')
+    if onlineHosts == 0:
+        doHelp("No hosts appear to be online", 4, verbose)
+    elif onlineHosts == 1:
+        p("One host appears to be online, please re-run this program with <ip> specified!", 'w')
+    else:
+        p("Multiple hosts appear to be online!, please re-run this program with <ip> specified!", 's')
 
 p("ToDo:")
+p("- Add CLA parser", 's')
+p("- Verify CLA variables", 's')
 p("- Scan network (or add host manually)")
 p("- Port scan host")
 p("- Select ports to fuzz, or all")
